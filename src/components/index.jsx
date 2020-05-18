4a3998a8886c075659b20518fb6f992ff5115837
Xin Lai <xinlai@Xins-MacBook-Pro-2.local>
2020-05-18 14:36:53 -0700

    first ini

---
+++ src/components/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Main extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        hello world!
+      </div>
+    );
+  }
+}
+
+export default Main;
