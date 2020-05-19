ac09e326e445c12763d05e29020e6c181c36f0fe
Xin Lai <xinlai@Xins-MacBook-Pro-2.local>
2020-05-18 18:55:09 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import injectSheet from 'react-jss';
+import Color from 'color';
 
-class App extends React.Component {
-  state = { };
 
+const styles = {
+  bar1Buffer: (props) => {
+    let color = props.buttonColor ? `#${props.buttonColor}` : '#080808';
+    if (color === '#000000') color = '#080808';
+    return { backgroundColor: [color, '!important'] };
+  },
+  bar2Buffer: (props) => {
+    let color = props.buttonColor ? `#${props.buttonColor}` : '#080808';
+    let lightenFactor = 0.3;
+    if (color === '#000000') {
+      color = '#080808';
+      lightenFactor = 6;
+    }
+    return { backgroundColor: [Color(color).lighten(lightenFactor).hex(), '!important'] };
+  }
+};
+
+
+class ProgressFullButton extends React.PureComponent {
   render() {
-    const { text, color } = this.props;
+    const {
+      idTxt,
+      buttonColor,
+      width,
+      classes
+    } = this.props;
+
+    const color = buttonColor ? `#${buttonColor}` : 'black';
+
     return (
-      <div>
-        Hello world
-      </div>
+      <LinearProgress
+        classes={{
+          bar1Buffer: classes.bar1Buffer,
+          bar2Buffer: classes.bar2Buffer,
+        }}
+        variant="buffer"
+        style={{ height: '3.25rem', bottom: 0, width: '100%' }}
+        value={width}
+        id={idTxt}
+      />
     );
   }
 }
 
-export default App;
+export default injectSheet(styles)(ProgressFullButton);
