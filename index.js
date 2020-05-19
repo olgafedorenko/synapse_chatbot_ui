3f25feab84525a186079f5cf62b5d3ae624a4ec8
Xin Lai <xinlai@Xins-MacBook-Pro-2.local>
2020-05-18 22:24:19 -0700

    last try

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+/* eslint-disable react/prefer-stateless-function */
 // eslint-disable-next-line import/no-unresolved
-module.exports = require('./dist');
+import React, { Component } from 'react';
+
+
+export class FullButton extends Component {
+  render() {
+    const { text, color } = this.props;
+    // eslint-disable-next-line react/jsx-filename-extension
+    return <h2>h1</h2>;
+  }
+}
+
+
+// eslint-disable-next-line react/prefer-stateless-function
+// eslint-disable-next-line react/no-multi-comp
+export class ProButton extends Component {
+  render() {
+    return <h2>hello</h2>;
+  }
+}
