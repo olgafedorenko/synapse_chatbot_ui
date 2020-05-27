a219e1031f98a36edd84451beccaabd868d6ece3
Xin Lai <xinlai@Xins-MacBook-Pro-2.local>
2020-05-26 17:52:57 -0700

    finish button

--- src/components/buttonRelated/Button.js
+++ src/components/buttonRelated/Button.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import hexToRgba from 'hex-to-rgba';
 
 const ButtonStyle = styled.button`
-width: 100%;
-height: 3.25rem;
+width: ${props => props.isFullButton ? '100%' : '90%'};
+height: ${props => props.isFullButton ? '3.25rem' : '3rem'};
 color: ${props => props.txtColor};
+border-radius: ${props => props.isFullButton ? null : '6px'};
+box-shadow: ${props => props.isFullButton ? `0 2px 4px 0 ${props => (props.disable ? '#757575' : hexToRgba(props.color, 0.6))}`: null};
 font-family: Nunito Sans;
 font-size: 1rem;
 font-weight: bold;
@@ -12,7 +15,7 @@ background-color: ${props => props.color};
 border: none;
 padding: 0 1rem;
 outline: none;
-transition: color, background-color;
+transition: ${props => props.isFullButton ? 'color, background-color' : 'color, background-color, box-shadow'};
 transition-duration: 0.3s;
 transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
 
@@ -20,70 +23,47 @@ ${props => props.style};
 ${props => props.styleDisabled};
 `;
 
-let transition;
-class FullButton extends React.PureComponent {
-  componentDidMount() {
-    const { idx, delayTxt } = this.props;
-    if (delayTxt) {
-      setTimeout(() => {
-        document.getElementById(idx).style.color = '#ffffff';
-      }, 1000);
-    }
-  }
+const Button = (props) => {
+  const {
+    id,
+    handleButtonSubmit,
+    type,
+    form,
+    buttonColor,
+    style,
+    isDisabled,
+		disableText,
+		isFullButton
+  } = props;
 
-  // componentDidUpdate(prevProps) {
-  //   const { idx, btnTxt, delayTxt } = this.props;
-  //   if (prevProps.btnTxt !== btnTxt) transition = true;
-  //   if (transition && delayTxt) {
-  //     document.getElementById(idx).style.color = 'transparent';
-  //     setTimeout(() => {
-  //       document.getElementById(idx).style.color = '#ffffff';
-  //       transition = false;
-  //     }, 1000);
-  //   }
-  // }
+	const styleDisabled = {
+		backgroundColor: '#757575',
+		cursor: 'unset',
+		outline: 0,
+		pointerEvents: 'none'
+	};
 
-  render() {
-    const {
-      idx,
-      handleButtonSubmit,
-      btnTxt,
-      type,
-      form,
-      buttonColor,
-      style,
-      disable,
-      disableText,
-      delayTxt
-    } = this.props;
+	const text = isDisabled ? disableText : props.children;
 
-    const styleDisabled = {
-      backgroundColor: '#757575',
-      cursor: 'unset',
-      outline: 0,
-      pointerEvents: 'none'
-    };
-
-    return (
-      <ButtonStyle
-        key={`inputboxBtn-${idx}`}
-        id={idx}
-        type={type}
-        btnTxt={btnTxt}
-        txtColor={delayTxt ? 'transparent' : '#ffffff'}
-        color={buttonColor ? `#${buttonColor}` : 'black'}
-        form={form}
-        className="fullButton"
-        onClick={disable ? null : handleButtonSubmit} // onMouseDown
-        disable={disable}
-        style={style}
-        styleDisabled={disable ? styleDisabled : null}
-      >
-        {disable ? disableText : btnTxt}
-        {/* {btnTxt === 'Secure bank login' ? <OpenNewWindowIcon fill={delayTxt ? 'transparent' : '#ffffff'} /> : null} */}
-      </ButtonStyle>
-    );
-  }
+  return (
+    <ButtonStyle
+			id={id}
+			key={`inputboxBtn-${id}`}
+			type={type}
+			txtColor={buttonColor === '#ffffff' ? 'black' : '#ffffff'}
+			color={buttonColor ? `#${buttonColor}` : 'black'}
+			form={form}
+			className="fullButton"
+			isDisabled={isDisabled}
+			onClick={isDisabled ? null : handleButtonSubmit} // onMouseDown
+			style={style}
+			styleDisabled={isDisabled ? styleDisabled : null}
+			isFullButton={isFullButton}
+  >
+		{text}
+    {/* {text === 'Secure bank login' ? <OpenNewWindowIcon fill={delayTxt ? 'transparent' : '#ffffff'} /> : null} */}
+  </ButtonStyle>
+  )
 }
 
-export default FullButton;
+export default Button;
